eddd825534ba2aaf7cc06bbe21765a6521f8240c
thilankal <thilanka.nuwan89@gmail.com>
2022-07-30 15:50:49 +0530

    dark mode form

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,13 @@
 <script lang="ts">
   import Counter from "./lib/Counter.svelte";
   import { onMount } from "svelte";
+import Form from "./pages/Form.svelte";
   // import './app.css'
 
   let isDark;
   let mainMount
+
+  $: title = isDark ? "Dark Mode" : "Light Mode";
 
   onMount(() => {
     // applyTheme(DEFAULT_THEME)
@@ -20,19 +23,23 @@
 <style lang="scss">
 
 :global(.theme-light) {
-  --text: theme(colors.gray.900);
+  // --text: theme(colors.gray.900);
   --bg-color: theme(colors.white);
-  --primary: theme(colors.red.900);
-  --primary-light: theme(colors.red.100);
-  --primary-bg: #ebece9;
+  --primary: #000000;
+  --primary-light: #ffffff;
+  --primary-bg: #ffffff;
+  --primary-btn: var(--primary);
+  --primary-btn-txt: var(--primary-bg);
   --text-color: #2c2d28;
 }
 
 :global(.theme-dark) {
-  --text: theme(colors.gray.100);
+  // --text: theme(colors.gray.100);
   --primary: #40413a;
   --primary-light: #52534b;
   --primary-bg: rgb(33, 37, 43);
+  --primary-btn: var(--primary-light);
+  --primary-btn-txt: var(--text-color);
   // --text-color: #ebece9;
   --text-color: #cccccc;
 }
@@ -44,20 +51,30 @@
   .super-parent {
     // background-color: var(--bg-color);
   }
+
+  button { 
+        width: 10rem;
+        background-color: var(--primary-btn);
+        color: var(--primary-btn-txt);
+        // margin-left: 0.6em;
+        // margin-top: 0.3em;
+    }
 </style>
 
 <!-- <main class="super-parent bg-blue-50 h-screen dark:bg-blue-900"> -->
 <main bind:this={mainMount}
   class="super-parent theme-light h-screen bg-primary-bg text-text-color"
   class:theme-dark={isDark}>
-  <h1 class="text-5xl text-accent font-bold underline">FIX Tester</h1>
+  <h1 class="text-5xl text-accent font-bold underline">{title}</h1>
 
-  <p class="text-5xl">Form</p>
+  <p class="text-3xl m-4">Form</p>
 
-  <button class="" on:click={changeTheme}>Change Theme</button>
+  
+  <Form />
+  <button class="m-2" on:click={changeTheme}>Change Theme</button>
 
   <div
     class="card bg-slate-500 shadow-md shadow-slate-700 rounded-lg drop-shadow-lg">
   </div>
-  <Counter />
+  <!-- <Counter /> -->
 </main>
---
+++ src/pages/Form.svelte
@@ -0,0 +1,52 @@
+<form>
+  <div class="form-container">
+    <div class="field-row">
+      <label for="">Message Name</label>
+      <input type="text" />
+    </div>
+    <div class="field-row">
+      <label for="">Qty</label>
+      <input type="text" />
+    </div>
+    <div class="action-row">
+      <button>Clear</button>
+      <button>Send</button>
+    </div>
+  </div>
+</form>
+
+<style lang="scss">
+  .form-container {
+    width: 35rem;
+  }
+  .field-row {
+    display: flex;
+    padding: 0.2em;
+    
+    label {
+      width: 10rem;
+      text-align: right;
+      padding-right: 0.5em;
+    }
+}
+
+.action-row {
+    display: flex;
+    padding: 0.2em;
+    justify-content: flex-end;
+    button { 
+        width: 4rem;
+        background-color: var(--primary-btn);
+        color: var(--primary-btn-txt);
+        margin-left: 0.6em;
+        margin-top: 0.3em;
+    }
+  }
+
+  input {
+    flex: 1;
+    background-color: var(--primary-light);
+    border-width: 0.08rem;
+    border-color: var(--primary);
+  }
+</style>
